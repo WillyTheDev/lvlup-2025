3ab51aeefcb257a4533a06fd700a51d4171e3822
Carolina Pereira <carolper2000@gmail.com>
2025-02-16 04:03:28 +0100

    feat: add cross to stack when no more cards

--- GameElements/CardManager/e_button.gd
+++ GameElements/CardManager/e_button.gd
@@ -3,6 +3,10 @@ extends Node2D
 const ACTIVE_TEXTURE = preload("res://Assets/UI/buttonE_active.png")
 const NEUTRAL_TEXTURE = preload("res://Assets/UI/buttonE_neutral.png")
 
+var ACTIVE_COLOR = Color.hex(0xc30a0aff)
+var NEUTRAL_COLOR = Color.hex(0x000000ff)
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,6 +16,8 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("use_card"):
 		self.get_node("Front").texture = ACTIVE_TEXTURE
+		self.get_parent().get_node("Cross").modulate = ACTIVE_COLOR
 	
 	if Input.is_action_just_released("use_card"):
 		self.get_node("Front").texture = NEUTRAL_TEXTURE
+		self.get_parent().get_node("Cross").modulate = NEUTRAL_COLOR
