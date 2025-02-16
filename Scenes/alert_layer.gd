1afc2ac8afe95c842943db9f856f934115534d71
Carolina Pereira <carolper2000@gmail.com>
2025-02-16 06:59:54 +0100

    feat: music and restore max cards + hide peek button

--- Scenes/alert_layer.gd
+++ Scenes/alert_layer.gd
@@ -3,6 +3,8 @@ extends CanvasLayer
 func show_layer():
 	self.visible = true
 	%AnimationPlayer.play("alert")
+	%AudioStreamPlayer.stream = preload("res://Assets/Music/Run Fast They Are Coming (with siren).mp3")
+	%AudioStreamPlayer.play()
 
 func stop_layer():
 	self.visible = false
